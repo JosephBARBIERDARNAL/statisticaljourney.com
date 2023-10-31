692e91e5eee3227f729b91c05a23db8950f8be1f
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2023-10-31 20:20:23 +0100

    tuto section in progress

--- other/histplottutorials.R
+++ other/histplottutorials.R
@@ -5,8 +5,10 @@ data <- data.frame(values = rnorm(10000))
 
 # Create a histogram with bins and density chart
 ggplot(data, aes(x = values)) +
-  geom_histogram(aes(y = ..density..), binwidth = 0.15, fill = "lightblue", color = "black") +
+  geom_histogram(aes(y = ..density..), binwidth = 0.4, fill = "lightblue", color = "black") +
   geom_density(color = "black") +
   theme_void() +  # Remove background grids, axis lines, and labels
   scale_x_continuous(breaks = seq(-3, 3, by = 0.1)) +  # Adjust x-axis breaks
-  labs(title = NULL, x = NULL, y = NULL)  # Remove title and axis labels
+  labs(title = NULL, x = NULL, y = NULL)  + # Remove title and axis labels
+  theme(panel.background = element_rect(fill = "#FDFDFD"))
+
